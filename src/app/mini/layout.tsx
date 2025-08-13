bf163c7581a045b9d569bdc15ba308843fc7372a
OneTrueHomie <vicigwilo@gmail.com>
2025-08-13 21:40:57 +0100

    trying to work on miniapp

--- src/app/mini/layout.tsx
+++ src/app/mini/layout.tsx
@@ -1,10 +1,13 @@
 // src/app/mini/layout.tsx
 import React from 'react'
 import { EnhancedProviders } from '@/components/providers/MiniKitProvider'
+import MiniAppReady from '@/components/miniapp/MiniAppReady'
 
 export default function MiniLayout({ children }: { children: React.ReactNode }): React.ReactElement {
 	return (
 		<EnhancedProviders>
+			{/* Ensure readiness is signaled immediately when embedded */}
+			<MiniAppReady />
 			{children}
 		</EnhancedProviders>
 	)
